f85a9f84acbbeed11b675c955a00cb0c1c90dcb2
BEN SALAH Ryma <ryma.bensalah@soprahr.com>
2023-05-25 15:54:33 +0200

    update api-gateway commit

--- src/main/java/com/gateway/apigateway/config/SpringCloudGatewayRouting.java
+++ src/main/java/com/gateway/apigateway/config/SpringCloudGatewayRouting.java
@@ -4,14 +4,21 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+@CrossOrigin("http://localhost:3000/")
 public class SpringCloudGatewayRouting {
     @Bean
     public RouteLocator configureRoute(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("user-service", r->r.path("/api/auth/**").uri("http://localhost:8089")) //static routing
-                .route("event-service", r->r.path("/api/event/**").uri("http://localhost:8088")) //dynamic routing
+                .route("user-service", r->r.path("/api/auth/**").uri("http://localhost:8089"))
+                .route("user-service", r->r.path("/api/admin/**").uri("http://localhost:8089")) //static routing
+                .route("user-service", r->r.path("/api/forgotpwd/**").uri("http://localhost:8089")) //static routing
+                .route("event-service", r->r.path("/api/**").uri("http://localhost:8088")) //dynamic routing
+                .route("event-service", r->r.path("/api/tag").uri("http://localhost:8088")) //dynamic routing
+                .route("event-service", r->r.path("/notification/**").uri("http://localhost:8088")) //dynamic routing
+                .route("event-service", r->r.path("/push-notifications/**").uri("http://localhost:8088")) //dynamic routing
                 .build();
     }
 }
